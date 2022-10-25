6886c910d3975a9b1f4aa0c7e7e53a5cb57a2726
Haoling <maya@fei-yen.jp>
2022-10-25 18:28:17 +0900

    commit

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
-set -o errexit
+set -x
+if [ -e ${DOCKER_SOCK} ]; then
+    DOCKER_GID=$(stat -c '%g' ${DOCKER_SOCK})
+    egrep "${DOCKER_GID}" /etc/group >&/dev/null
+    if [ $? -ne 0 ]; then
+        groupadd -g ${DOCKER_GID} docker
+    fi
+    usermod -G docker jenkins
+else
+    echo "Agent is not allow run docker command! if you want do it, please mount '/var/run/docker.sock' file."
+fi
+chown -R jenkins.jenkins /home/jenkins
 
-if [ -n "${SWARM_ENV_FILE}" ]; then
-  source ${SWARM_ENV_FILE}
+if [ ! -z $http_proxy ] && [ ! -z $https_proxy ]; then
+    git config --global http.proxy $http_proxy
+    git config --global https.proxy $https_proxy
 fi
 
-docker_remote_registry=""
-
-if [ -n "${DOCKER_REGISTRY}" ]; then
-  docker_remote_registry=${DOCKER_REGISTRY}
-fi
-
-docker_user=""
-docker_user_email=""
-docker_user_password=""
-
-if [ -n "${DOCKER_REGISTRY_USER}" ]; then
-  docker_user=${DOCKER_REGISTRY_USER}
-  docker_user_email=${DOCKER_REGISTRY_EMAIL}
-  docker_user_password=${DOCKER_REGISTRY_PASSWORD}
-  unset DOCKER_REGISTRY_USER
-  unset DOCKER_REGISTRY_EMAIL
-  unset DOCKER_REGISTRY_PASSWORD
-  docker login --username=${docker_user} --email=${docker_user_email} --password=${docker_user_password} ${docker_remote_registry}
-fi
-
-${SWARM_HOME}/docker-entrypoint.sh $@
+exec gosu jenkins "$@"
